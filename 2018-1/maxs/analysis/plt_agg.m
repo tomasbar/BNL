d42befae7cbf7f2f26161f25a713a3950d332062
Tomas <tomas.bar94@gmail.com>
2018-04-30 23:47:07 -0400

    Succesfully plotted q and qr

--- 2018-1/maxs/analysis/plt_agg.m
+++ 2018-1/maxs/analysis/plt_agg.m
@@ -5,9 +5,20 @@ clear all
 close all
 clc
 
+%% Set up, clean files
+
 if isfolder('./processed_imgs') == 0
-    mkdir('./processed)imgs')
+    mkdir('./processed_imgs')
 end
+
+if isfolder('./processed_imgs/horiz') == 0
+    mkdir('./processed_imgs/horiz')
+end
+
+if isfolder('./processed_imgs/comparisons') == 0
+    mkdir('./processed_imgs/comparisons')
+end
+
 %% PLOT MAPLE
 
 %Sample identifiers
@@ -33,6 +44,7 @@ for ii = 1:length(ID_MAPLE)
     filenames_NA = string({img_NA.name});
     filenames_ANNEAL = string({img_ANNEAL.name});
     
+    %Block
     f1 = figure;
     f2 = figure;
     
@@ -44,12 +56,149 @@ for ii = 1:length(ID_MAPLE)
     montage(filenames_ANNEAL,'Size',[2 4],'BackgroundColor','w')
     title(sprintf('MAPLE%c 110%cC Anneal',ID_MAPLE(ii),char(176)))
     
-    print(sprintf('./processed_imgs/MAPLE%c_NA.png',ID_MAPLE(ii)),'-dpng','-r200','-f1')
-    print(sprintf('./processed_imgs/MAPLE%c_110C.png',ID_MAPLE(ii)),'-dpng','-r200','-f2')
+    print(sprintf('./processed_imgs/MAPLE%c_NA.png',ID_MAPLE(ii)),'-dpng','-r300','-f1')
+    print(sprintf('./processed_imgs/MAPLE%c_110C.png',ID_MAPLE(ii)),'-dpng','-r300','-f2')
     close all
+    clear -regexp \d
+    
+    %Horiz
+    f1 = figure;
+    f2 = figure;
+    
+    figure(f1)
+    montage(filenames_NA,'Size',[1 8],'BackgroundColor','w')
+    title(sprintf('MAPLE%c No Anneal',ID_MAPLE(ii)))
+    
+    figure(f2)
+    montage(filenames_ANNEAL,'Size',[1 8],'BackgroundColor','w')
+    title(sprintf('MAPLE%c 110%cC Anneal',ID_MAPLE(ii),char(176)))
+   
+    
+    print(sprintf('./processed_imgs/horiz/MAPLE%c_NA_horiz.png',ID_MAPLE(ii)),'-dpng','-r300','-f1')
+    print(sprintf('./processed_imgs/horiz/MAPLE%c_110C_horiz.png',ID_MAPLE(ii)),'-dpng','-r300','-f2')
+
+    
+    close all
+    clear f*
+    
+    %Comparison
+    f1 = figure;
+   
+    img_comp = dir(fullfile('./processed_imgs/horiz/',sprintf('MAPLE%c*horiz*',ID_MAPLE(ii))));
+    filenames_comp = string({img_comp.name});
+    
+    figure(f1)
+    montage(filenames_comp,'Size',[2 1],'BackgroundColor','w')
+        
+    print(sprintf('./processed_imgs/comparisons/MAPLE%c_comp.png',ID_MAPLE(ii)),'-dpng','-r300','-f1')
+        
+    close all
+    clear f*
 end
 
+%% PLOT SPINCAST
 
+%Sample identifiers
+ID_SPIN= ['A','B','C','D'];
+
+%Step thru MAPLE samples, by identifier in ID_MAPLE
+for ii = 1:length(ID_SPIN)
+    
+    %Load sample filenames to be plotted
+    img_MEG_NA = dir(sprintf('SPIN%c_MEG_NA*',ID_SPIN(ii)));
+    img_MEG_ANNEAL = dir(sprintf('SPIN%c_MEG_110C*',ID_SPIN(ii)));
+    img_noMEG_NA = dir(sprintf('SPIN%c_NoMEG_NA*',ID_SPIN(ii)));
+    img_noMEG_ANNEAL = dir(sprintf('SPIN%c_NoMEG_110C*',ID_SPIN(ii)));
+    
+    if length(img_MEG_NA) > 8
+        error('TOO MANY DATA POINTS, CHECK FOR DUPLICATES')
+        exit
+    end
+    
+    if length(img_MEG_ANNEAL) > 8
+        error('TOO MANY DATA POINTS, CHECK FOR DUPLICATES')
+        exit
+    end
+    
+    filenames_MEG_NA = string({img_MEG_NA.name});
+    filenames_MEG_ANNEAL = string({img_MEG_ANNEAL.name});
+    filenames_noMEG_NA = string({img_noMEG_NA.name});
+    filenames_noMEG_ANNEAL = string({img_noMEG_ANNEAL.name});
+    
+    %Block
+    f1 = figure;
+    f2 = figure;
+    f3 = figure;
+    f4 = figure;
+    
+    figure(f1)
+    montage(filenames_MEG_NA,'Size',[2 4],'BackgroundColor','w')
+    title(sprintf('SPIN%c MEG No Anneal',ID_SPIN(ii)))
+    
+    figure(f2)
+    montage(filenames_MEG_ANNEAL,'Size',[2 4],'BackgroundColor','w')
+    title(sprintf('SPIN%c MEG 110%cC Anneal',ID_SPIN(ii),char(176)))
+    
+    figure(f3)
+    montage(filenames_noMEG_NA,'Size',[2 4],'BackgroundColor','w')
+    title(sprintf('SPIN%c No MEG No Anneal',ID_SPIN(ii)))
+    
+    figure(f4)
+    montage(filenames_noMEG_ANNEAL,'Size',[2 4],'BackgroundColor','w')
+    title(sprintf('SPIN%c No MEG 110%cC Anneal',ID_SPIN(ii),char(176)))
+    
+    print(sprintf('./processed_imgs/SPIN%c_MEG_NA.png',ID_SPIN(ii)),'-r300','-dpng','-f1')
+    print(sprintf('./processed_imgs/SPIN%c_MEG_110C.png',ID_SPIN(ii)),'-r300','-dpng','-f2')
+    print(sprintf('./processed_imgs/SPIN%c_noMEG_NA.png',ID_SPIN(ii)),'-r300','-dpng','-f3')
+    print(sprintf('./processed_imgs/SPIN%c_noMEG_110C.png',ID_SPIN(ii)),'-r300','-dpng','-f4')
+    
+    close all
+    clear -regexp \d
+    
+    %Horiz
+    f1 = figure;
+    f2 = figure;
+    f3 = figure;
+    f4 = figure;
+    
+    figure(f1)
+    montage(filenames_MEG_NA,'Size',[1 8],'BackgroundColor','w')
+    title(sprintf('SPIN%c MEG No Anneal',ID_SPIN(ii)))
+    
+    figure(f2)
+    montage(filenames_MEG_ANNEAL,'Size',[1 8],'BackgroundColor','w')
+    title(sprintf('SPIN%c MEG 110%cC Anneal',ID_SPIN(ii),char(176)))
+    
+    figure(f3)
+    montage(filenames_noMEG_NA,'Size',[1 8],'BackgroundColor','w')
+    title(sprintf('SPIN%c No MEG No Anneal',ID_SPIN(ii)))
+    
+    figure(f4)
+    montage(filenames_noMEG_ANNEAL,'Size',[1 8],'BackgroundColor','w')
+    title(sprintf('SPIN%c No MEG 110%cC Anneal',ID_SPIN(ii),char(176)))
+    
+    print(sprintf('./processed_imgs/horiz/SPIN%c_MEG_NA_horiz.png',ID_SPIN(ii)),'-dpng','-r300','-f1')
+    print(sprintf('./processed_imgs/horiz/SPIN%c_MEG_110C_horiz.png',ID_SPIN(ii)),'-r300','-dpng','-f2')
+    print(sprintf('./processed_imgs/horiz/SPIN%c_noMEG_NA_horiz.png',ID_SPIN(ii)),'-r300','-dpng','-f3')
+    print(sprintf('./processed_imgs/horiz/SPIN%c_noMEG_110C_horiz.png',ID_SPIN(ii)),'-r300','-dpng','-f4')
+    
+    close all
+    clear f*
+    
+    %Comparison
+    f1 = figure;
+   
+    img_comp = dir(fullfile('./processed_imgs/horiz/',sprintf('SPIN%c*horiz*',ID_SPIN(ii))));
+    filenames_comp = string({img_comp.name});
+    
+    figure(f1)
+    montage(filenames_comp,'Size',[4 1],'BackgroundColor','w')
+        
+    print(sprintf('./processed_imgs/comparisons/SPIN%c_comp.png',ID_SPIN(ii)),'-r300','-dpng','-f1')
+        
+    close all
+    clear f*
+end
 
 
 
