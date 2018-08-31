f26a2b10277e276a707aad62dc5d7ef18ed481b7
tomas <tomas@Desky.nc.rr.com>
2018-08-31 00:58:16 -0400

    spin AL plots

--- 2018-1/maxs/analysis/linecut_angle/plotLA_spin.m
+++ 2018-1/maxs/analysis/linecut_angle/plotLA_spin.m
@@ -6,69 +6,69 @@ f3 = figure;
 f4 = figure;
 
 figure(f1)
-plot(MAPLE2_NA.chi,MAPLE2_NA.I)
+plot(SpinA_noMEG_NA.chi,SpinA_noMEG_NA.I)
 hold on
 
-plot(MAPLE3_NA.chi,MAPLE3_NA.I)
-plot(MAPLE4_NA.chi,MAPLE4_NA.I)
-plot(MAPLE5_NA.chi,MAPLE5_NA.I)
+plot(SpinB_noMEG_NA.chi,SpinB_noMEG_NA.I)
+plot(SpinC_noMEG_NA.chi,SpinC_noMEG_NA.I)
+plot(SpinD_noMEG_NA.chi,SpinD_noMEG_NA.I)
 hold off
 
 figure(f2)
-plot(MAPLE2_110C.chi,MAPLE2_110C.I)
+plot(SpinA_noMEG_110.chi,SpinA_noMEG_110.I)
 hold on
 
-plot(MAPLE3_110C.chi,MAPLE3_110C.I)
-plot(MAPLE4_110C.chi,MAPLE4_110C.I)
-plot(MAPLE5_110C.chi,MAPLE5_110C.I)
+plot(SpinB_noMEG_110.chi,SpinB_noMEG_110.I)
+plot(SpinC_noMEG_110.chi,SpinC_noMEG_110.I)
+plot(SpinD_noMEG_110.chi,SpinD_noMEG_110.I)
 hold off
 
 figure(f3)
-plot(MAPLE2_NA.chi,MAPLE2_NA.I.*sind(MAPLE2_NA.chi))
+plot(SpinA_noMEG_NA.chi,SpinA_noMEG_NA.I.*sind(SpinA_noMEG_NA.chi))
 hold on
 
-plot(MAPLE3_NA.chi,MAPLE3_NA.I.*sind(MAPLE3_NA.chi))
-plot(MAPLE4_NA.chi,MAPLE4_NA.I.*sind(MAPLE4_NA.chi))
-plot(MAPLE5_NA.chi,MAPLE5_NA.I.*sind(MAPLE5_NA.chi))
+plot(SpinB_noMEG_NA.chi,SpinB_noMEG_NA.I.*sind(SpinB_noMEG_NA.chi))
+plot(SpinC_noMEG_NA.chi,SpinC_noMEG_NA.I.*sind(SpinC_noMEG_NA.chi))
+plot(SpinD_noMEG_NA.chi,SpinD_noMEG_NA.I.*sind(SpinD_noMEG_NA.chi))
 hold off
 
 figure(f4)
-plot(MAPLE2_110C.chi,MAPLE2_110C.I.*sind(MAPLE2_110C.chi))
+plot(SpinA_noMEG_110.chi,SpinA_noMEG_110.I.*sind(SpinA_noMEG_110.chi))
 hold on
 
-plot(MAPLE3_110C.chi,MAPLE3_110C.I.*sind(MAPLE3_110C.chi))
-plot(MAPLE4_110C.chi,MAPLE4_110C.I.*sind(MAPLE4_110C.chi))
-plot(MAPLE5_110C.chi,MAPLE5_110C.I.*sind(MAPLE5_110C.chi))
+plot(SpinB_noMEG_110.chi,SpinB_noMEG_110.I.*sind(SpinB_noMEG_110.chi))
+plot(SpinC_noMEG_110.chi,SpinC_noMEG_110.I.*sind(SpinC_noMEG_110.chi))
+plot(SpinD_noMEG_110.chi,SpinD_noMEG_110.I.*sind(SpinD_noMEG_110.chi))
 hold off
 
 figure(f1)
 hold off
-title('MAPLE No Anneal Azimuthal Linecut')
-legend('MAPLE2','MAPLE3','MAPLE4','MAPLE5')
+title('Spin no MEG No Anneal q=1 Azimuthal Linecut')
+legend('SpinA','SpinB','SpinC','SpinD')
 % xlim([0 inf])
-savefig('./processed_imgs/MAPLE_NA_AL')
-print('./processed_imgs/MAPLE_NA_AL','-dpng','-r300','-f1')
+savefig('./processed_imgs/Spin_noMEG_NA_AL')
+print('./processed_imgs/Spin_noMEG_NA_AL','-dpng','-r300','-f1')
 
 figure(f2)
 hold off
-title('MAPLE 110C Azimuthal Linecut')
-legend('MAPLE2','MAPLE3','MAPLE4','MAPLE5')
+title('Spin no MEG 110C q=1 Azimuthal Linecut')
+legend('SpinA','SpinB','SpinC','SpinD')
 % xlim([0 inf])
-savefig('./processed_imgs/MAPLE_110C_AL')
-print('./processed_imgs/MAPLE_110C_AL','-dpng','-r300','-f2')
+savefig('./processed_imgs/Spin_noMEG_110C_AL')
+print('./processed_imgs/Spin_noMEG_110C_AL','-dpng','-r300','-f2')
 
 figure(f3)
 hold off
-title('MAPLE No Anneal Azimuthal Linecut (sin normalized)')
-legend('MAPLE2','MAPLE3','MAPLE4','MAPLE5')
+title('Spin no MEG No Anneal q=1 Azimuthal Linecut (sin normalized)')
+legend('SpinA','SpinB','SpinC','SpinD')
 xlim([0 inf])
-savefig('./processed_imgs/MAPLE_NA_sin_AL')
-print('./processed_imgs/MAPLE_NA_sin_AL','-dpng','-r300','-f3')
+savefig('./processed_imgs/Spin_noMEG_NA_sin_AL')
+print('./processed_imgs/Spin_noMEG_NA_sin_AL','-dpng','-r300','-f3')
 
 figure(f4)
 hold off
-title('MAPLE 110C Azimuthal Linecut (sin normalized)')
-legend('MAPLE2','MAPLE3','MAPLE4','MAPLE5')
+title('Spin no MEG 110C q=1 Azimuthal Linecut (sin normalized)')
+legend('SpinA','SpinB','SpinC','SpinD')
 xlim([0 inf])
-savefig('./processed_imgs/MAPLE_110C_sin_AL')
-print('./processed_imgs/MAPLE_110C_sin_AL','-dpng','-r300','-f4')
+savefig('./processed_imgs/Spin_noMEG_110C_sin_AL')
+print('./processed_imgs/Spin_noMEG_110C_sin_AL','-dpng','-r300','-f4')
